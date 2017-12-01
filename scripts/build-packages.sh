d4a7e3f0afc2fc5d450b2042bd6764d6172fcc16
Tony Edgin <tedgin@iplantcollaborative.org>
2017-12-01 13:10:25 -0700

    DS-217 converted body of build-packages.sh to function

--- scripts/build-packages.sh
+++ scripts/build-packages.sh
@@ -1,35 +1,47 @@
 #!/bin/bash
 
-readonly OS="$1"
 
-mkdir --parents /packages/"$OS"
+main()
+{
+  local os="$1"
 
-for pkg in api microservices icommands
-do
-  /src/"$pkg"/packaging/build.sh -r
-  cp --update /src/"$pkg"/build/* /packages/"$OS"
-done
+  mkdir --parents /packages/"$os"
 
-readonly LibDir=/usr/lib
+  for pkg in api microservices icommands
+  do
+    /src/"$pkg"/packaging/build.sh -r
+    cp --update /src/"$pkg"/build/* /packages/"$os"
+  done
 
-case "$OS"
-in
-  centos-6)
-    pkgId=centos6
-    ;;
-  centos-7)
-    pkgId=centos7
-    ;;
-  ubuntu-12)
-    pkgId=ubuntu12
-    ;;
-  ubuntu-14)
-    pkgId=ubuntu14
-    ;;    
-esac
+  local pkgId=
 
-# Gather irods-runtime contents
-mkdir --parents /src/tar/irods/externals
-cp --no-dereference --update "$LibDir"/libirods*.so* /src/tar
-cp --no-dereference --update "$LibDir"/irods/externals/*.so* /src/tar/irods/externals
-tar --create --gzip --directory /src/tar --file /packages/"$OS"/irods-runtime-4.1.10-"$pkgId".tgz .
+  case "$os"
+  in
+    centos-6)
+      pkgId=centos6
+      ;;
+    centos-7)
+      pkgId=centos7
+      ;;
+    ubuntu-12)
+      pkgId=ubuntu12
+      ;;
+    ubuntu-14)
+      pkgId=ubuntu14
+      ;;    
+  esac
+
+  # Gather irods-runtime contents
+  mkdir --parents /src/tar/irods/externals
+  cp --no-dereference --update /usr/lib/libirods*.so* /src/tar
+  cp --no-dereference --update /usr/lib/irods/externals/*.so* /src/tar/irods/externals
+
+  tar --create --gzip \
+      --directory /src/tar --file /packages/"$os"/irods-runtime-4.1.10-"$pkgId".tgz \
+      .
+}
+
+
+set -e
+
+main "$*"
